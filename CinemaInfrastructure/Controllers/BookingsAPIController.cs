b9ac06025c66d645005f26bed7458c5f85b80f3f
unknown <anhelina.pryadko@gmail.com>
2025-10-16 22:23:12 +0300

    add BookingAPIController without testing

---
+++ CinemaInfrastructure/Controllers/BookingsAPIController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CinemaDomain.Model;
+using CinemaInfrastructure;
+
+namespace CinemaInfrastructure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsAPIController : ControllerBase
+    {
+        private readonly CinemaContext _context;
+
+        private async Task<bool> BookingExistsAsync(int sessionId, int seatId)
+        {
+            return await _context.Bookings.AnyAsync(b =>
+                        b.SessionId == sessionId
+                        && b.SeatId == seatId);
+        }
+
+        public BookingsAPIController(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BookingsAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Booking>>> GetBookings()
+        {
+            return await _context.Bookings
+                .Include(b => b.Seat)
+                    .ThenInclude(s => s.Hall)
+                        .ThenInclude(h => h.HallType)
+                .Include(b => b.Session)
+                    .ThenInclude(s => s.Film)
+                        .ThenInclude(f => f.FilmCategory)
+                .Include(b => b.Session)
+                    .ThenInclude(s => s.Film)
+                        .ThenInclude(f => f.Company)
+                .Include(b => b.Viewer)
+                .ToListAsync();
+        }
+
+        // GET: api/BookingsAPI/1/10/2 (ViewerId, SessionId, SeatId)
+        [HttpGet("{viewerId}/{sessionId}/{seatId}")]
+        public async Task<ActionResult<Booking>> GetBooking(int viewerId, int sessionId, int seatId)
+        {
+            var booking = await _context.Bookings
+                .Include(b => b.Viewer)
+                .Include(b => b.Seat)
+                    .ThenInclude(b => b.Hall)
+                        .ThenInclude(h => h.HallType)
+                .Include(b => b.Session)
+                    .ThenInclude(s => s.Film)
+                        .ThenInclude(f => f.FilmCategory)
+                .Include(b => b.Session)
+                    .ThenInclude(s => s.Film)
+                        .ThenInclude(f => f.Company)
+                .FirstOrDefaultAsync(
+                    b =>
+                        b.ViewerId == viewerId
+                        && b.SessionId == sessionId
+                        && b.SeatId == seatId
+                );
+
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return booking;
+        }
+
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // PUT: api/BookingsApi/1/10/2 (ViewerId, SessionId, SeatId)
+        [HttpPut("{viewerId}/{sessionId}/{seatId}")]
+        public async Task<IActionResult> PutBooking(int viewerId, int sessionId, int seatId, Booking booking)
+        {
+            if (viewerId != booking.ViewerId || sessionId != booking.SessionId)
+            {
+                return BadRequest(new { Error = "В бронюванні можливо змінити лише місце на обраний сеанс" });
+            }
+
+            var oldBooking = await _context.Bookings.FindAsync(viewerId, sessionId, seatId);
+            if (oldBooking == null)
+            {
+                return NotFound(new { Error = "Бронювання не знайдено." });
+            }
+
+            if (oldBooking.SeatId == booking.SeatId)
+            {           
+                return Ok(new { Status = "Ok", Message = "Бронювання місця не вимагає оновлення." });
+            }
+
+            if (await BookingExistsAsync(sessionId, booking.SeatId))
+            {
+                return BadRequest(new { Error = "Обране місце вже заброньовано!" });
+            }
+
+            _context.Bookings.Remove(oldBooking);
+            var newBooking = new Booking
+            {
+                ViewerId = viewerId,
+                SessionId = sessionId,
+                SeatId = booking.SeatId
+            };
+            _context.Bookings.Add(newBooking);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return StatusCode(500, new { Error = "Помилка при збереженні нового бронювання.", Details = ex.Message });
+            }
+            return Ok(new { Status = "Ok" });
+        }
+
+        // POST: api/BookingsAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Booking>> PostBooking(Booking booking)
+        {
+            if(await BookingExistsAsync(booking.SessionId, booking.SeatId))
+            {
+                return BadRequest(new { Error = "Обране місце вже заброньовано!" });
+            }
+
+            var newBooking = new Booking
+            {
+                ViewerId = booking.ViewerId,
+                SessionId = booking.SessionId,
+                SeatId = booking.SeatId
+            };
+
+            _context.Bookings.Add(newBooking);
+            try
+            {
+                await _context.SaveChangesAsync();
+            } 
+            catch(DbUpdateConcurrencyException ex)
+            {
+                return StatusCode(500, new { Error = "Помилка при збереженні нового бронювання.", Details = ex.Message });
+            }
+            return CreatedAtAction("GetBooking",
+                new { viewerId = booking.ViewerId, sessionId = booking.SessionId, seatId = booking.SeatId },
+                new { Status = "Ok" }); // Змінено на "Ok" згідно з вашим завданням
+        }
+
+        // DELETE: api/BookingsAPI/1/10/2 (ViewerId, SessionId, SeatId)
+        [HttpDelete("{viewerId}/{sessionId}/{seatId}")]
+        public async Task<IActionResult> DeleteBooking(int viewerId, int sessionId, int seatId)
+        {
+            var booking = await _context.Bookings.FindAsync(viewerId, sessionId, seatId);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bookings.Remove(booking);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return StatusCode(500, new { Error = "Помилка при збереженні нового бронювання.", Details = ex.Message });
+            }
+            return Ok(new { Status = "Ok" });
+        }
+    }
+}
